f957518aa7b88faaa8f5d916362a288bbbfc7659
Cry <alexanderkatsero@gmail.com>
2017-09-25 22:09:16 +0300

    dotenv & request

--- src/Comms.js
+++ src/Comms.js
@@ -1,11 +1,18 @@
+require('dotenv').config()
+
+const request = require('request')
+
 module.exports = class Comms {
   constructor (events) {
     this.events = events
+    this.host = process.env.HOST
+    this.port = process.env.PORT
 
     this.subscribe()
   }
 
   transmit (direction) {
+    request.get(`${this.host}:${this.port}/move/${direction}`)
     console.log('direction:', direction)
   }
 
